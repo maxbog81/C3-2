971e6414699b2de6b85d6db81785287d0aefe866
Maksim Bogatov <maxbog@inbox.ru>
2019-06-18 11:33:16 +0300

    Урок 2

--- WpfMailSender/WpfMailSender/TestData.cs
+++ WpfMailSender/WpfMailSender/TestData.cs
@@ -11,7 +11,7 @@ namespace WpfMailSender
         public static List<Server> Servers { get; } = new List<Server>
         {
             new Server{ Name = "Yandex", Address = "smtp.yandex.ru", Port = 25 },
-            new Server{ Name = "Mail.ru", Address = "smtp.mail.ru", Port = 2525 },
+            new Server{ Name = "Mail.ru", Address = "smtp.mail.ru", Port = 25 },
             new Server{ Name = "Gmail", Address = "smtp.gmail.com", Port = 25 }
         };
     }
